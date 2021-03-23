667f929eb8287ce96ea7e0cca3ef44b2f230a64e
qiaojingqiu <qiaojing@hawaii.edu>
2021-03-23 00:17:07 -1000

    assignment1

---
+++ qiu_qiaojing_Assignment1/products.js
@@ -0,0 +1,28 @@
+
+var product_array = [
+    {"name": "Personal Tax Return",
+   "price": 200,
+   "CPA_name": "Amy",
+   "image": "image/Personal.jpg"
+},
+    {"name": "Corporate Tax Return",
+    "price": 525,
+    "CPA_name": "John",
+    "image": "image/corporate.jpg"
+},
+    {"name": "Trust/Estate Tax Return",
+    "price": 415,
+    "CPA_name": "Kim",
+    "image": "image/trust.jpg"
+},
+    {"name": "State/Federal Tax Return",
+    "price": 100,
+    "CPA_name": "Sara",
+    "image": "image/state.jpg"
+},
+    {"name": "Tax Planning",
+    "price": 30,
+    "CPA_name": "Hannah",
+    "image": "image/planning.jpeg"
+}  
+]
